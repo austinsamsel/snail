9b499a2045c08eedc0cb4d0688bfa7056a01a317
austin samsel <austinsamsel@gmail.com>
2015-11-27 17:42:31 -0500

    add publish

--- model/meteor.jsx
+++ model/meteor.jsx
@@ -4,9 +4,18 @@ if (Meteor.isClient) {
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
   })
+
+  Meteor.subscribe("letters");
+
   Meteor.startup(function () {
     // Use Meteor.startup to render the component after the page is ready
     ReactDOM.render(<App />, document.getElementById("app"));
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.publish("letters", function() {
+    return Letters.find();
   });
 }
 
