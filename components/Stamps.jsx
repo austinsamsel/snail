eb82dd078ccae73cecae81b0fcbb5a722b4ae6c9
austin samsel <austinsamsel@gmail.com>
2016-01-18 13:45:30 -0500

    add stamps component, but not in use

---
+++ components/Stamps.jsx
@@ -0,0 +1,28 @@
+Stamps = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+
+  render(){
+    return (
+      <div className="stampBox">
+        <h1>Stamps</h1>
+        <StampList data={this.state.data} />
+      </div>
+    )
+  }
+
+})
